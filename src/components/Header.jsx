a3327e26098b43ea106c8c8d01f0983701ba565c
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-14 15:44:30 +0900

    header 기능 구현

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>COUNTER</li>
+      </nav>
+    </header>
+  );
+}
